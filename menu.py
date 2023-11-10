7cc2df81bbde63996dbf47e9a23f7b9277e03244
sonja <bsbaro@me.com>
2023-11-09 17:31:58 -0700

    updated code

--- menu.py
+++ menu.py
@@ -53,8 +53,6 @@ menu = {
 # 1. Set up order list. Order list will store a list of dictionaries for
 # menu item name, item price, and quantity ordered
 order_list = menu
-print "menu"
-
 # Launch the store and present a greeting to the customer
 print("Welcome to the variety food truck.")
 
